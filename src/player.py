42c9bc7b44330f45d5724beefb414ac41306122a
ubAI6689 <ub.ai6689@gmail.com>
2023-07-10 12:28:52 +0700

    ...

--- src/player.py
+++ src/player.py
@@ -2,12 +2,12 @@ class Player:
     def __init__(self, number):
         # Initialize the player with the given player number
         self.number = number
-        self.store = 13 if number == 1 else 6  # Store is at index 6 for player 1 and index 13 for player 2
+        self.store = 14 if number == 1 else 15  # Store is at index 14 for player 1 and index 15 for player 2
 
     def choose_house(self, board):
         # Choose a house to sow from
         # For simplicity, just choose the house with the most seeds
-        if self.player_number == 1:
+        if self.number == 1:
             houses = board.houses[0:6]
         else:
             houses = board.houses[7:13]
@@ -16,7 +16,7 @@ class Player:
         chosen_house = houses.index(max_seeds)
         
         # Adjust chosen_house index if player_number is 2
-        if self.player_number == 2:
+        if self.number == 2:
             chosen_house += 7
 
         return chosen_house
